191d38e8ab89cc2b2d8b45c80eb46049a73726c5
Oleksandr Panchenko <panch@DESKTOP-NH15BJE>
2022-02-21 22:57:30 +0200

    Added custom logger, added minimal api.

---
+++ Tic_Tac_Toe_Server/CustomLogger/LoggerExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace Tic_Tac_Toe.Server.CustomLogger
+{
+    public static class LoggerExtensions
+    {
+        public static ILoggingBuilder AddFile(this ILoggingBuilder factory, string path)
+        {
+            _ = factory ?? throw new ArgumentNullException(nameof(factory));
+
+            return factory.AddProvider(new LoggerProvider(path));
+        }
+
+    }
+}
---
+++ Tic_Tac_Toe_Server/Program.cs
@@ -0,0 +1,49 @@
+﻿using Tic_Tac_Toe.Server.CustomLogger;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var services = builder.Services;
+services.AddEndpointsApiExplorer();
+services.AddSwaggerGen();
+
+var host = builder.Host;
+
+host.ConfigureAppConfiguration(app =>
+{
+    app.AddJsonFile("appsettings.json", true, true)
+        .AddEnvironmentVariables();
+})
+.ConfigureLogging(loggerBuilder =>
+{
+    loggerBuilder.ClearProviders();
+    var filepath = Path.Combine(Directory.GetCurrentDirectory(), "app.log");
+    loggerBuilder.AddFile(filepath);
+
+})
+.ConfigureServices(services =>
+{
+    services.AddOptions();
+    services.AddControllers();
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseRouting();
+
+app.MapGet("/", () => "Hello World!");
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
+
+
+await app.RunAsync();
