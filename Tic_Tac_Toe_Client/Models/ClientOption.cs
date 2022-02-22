38312ff3e856d6fda8e59d304740ac4bcb31911b
Oleksandr Panchenko <panch@DESKTOP-NH15BJE>
2022-02-22 17:41:24 +0200

    Revert "Revert "Added login, simple menu""
    This reverts commit 1e6215abd34f927079723892b90f5bd94ab98323.

---
+++ Tic_Tac_Toe_Client/Models/ClientOption.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+
+namespace Tic_Tac_Toe.Client.Models
+{
+    internal class ClientOption
+    {
+        [JsonPropertyName("BaseAddress")]
+        public string UriAddress { get; set; }
+    }
+}
