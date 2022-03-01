2d8080079de825063cf98d2d3743ca3ac94814ea
Norbert <norbert.petah28@gmail.com>
2022-03-01 18:35:32 +0200

    Added drawing board

--- Tic_Tac_Toe_Client/ConsoleHelper.cs
+++ Tic_Tac_Toe_Client/ConsoleHelper.cs
@@ -18,10 +18,22 @@ namespace TicTacToe.Client
             Console.ForegroundColor = startColor;
         }
 
+        public static void WriteInConsole(string message,
+            ConsoleColor color)
+        {
+            var startColor = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+
+            Console.Write(message);
+
+            Console.ForegroundColor = startColor;
+        }
+
         public static void ReadIntFromConsole(out int choose)
         {
             choose = Convert.ToInt32(Console.ReadLine(), CultureInfo.CurrentCulture);
         }
+
         
     }
 }
--- Tic_Tac_Toe_Client/Models/Board.cs
+++ Tic_Tac_Toe_Client/Models/Board.cs
@@ -1,4 +1,5 @@
-﻿using TicTacToe.Client.DTO;
+﻿using System.Globalization;
+using TicTacToe.Client.DTO;
 
 namespace TicTacToe.Client.Models;
 
@@ -9,12 +10,34 @@ public class Board
 
     public void Draw()
     {
-        for (var i = 0; i <_boards.Length; i++)
-        {
-            if ( i % 3 == 0)
-                Console.WriteLine();
-            Console.Write(_boards[i] + " ");
-        }
+        DrawLineInBoard(_boards[0], ConsoleColor.Red,
+            _boards[1], ConsoleColor.Red,
+            _boards[2], ConsoleColor.Green);
+        Console.WriteLine("_____|_____|_____ ");
+
+        DrawLineInBoard(_boards[3], ConsoleColor.Green,
+            _boards[4], ConsoleColor.Red,
+            _boards[5], ConsoleColor.Green);
+        Console.WriteLine("_____|_____|_____ ");
+
+        DrawLineInBoard(_boards[6], ConsoleColor.Red,
+            _boards[7], ConsoleColor.Green,
+            _boards[8], ConsoleColor.Red);
+        Console.WriteLine("     |     |     ");
+    }
+
+    private void DrawLineInBoard(int x, ConsoleColor xColor,
+        int y, ConsoleColor yColor,
+        int z, ConsoleColor zColor)
+    {
+        Console.WriteLine("     |     |      ");
+        Console.Write("  ");
+        ConsoleHelper.WriteInConsole(x.ToString(CultureInfo.InvariantCulture), xColor);
+        Console.Write("  |  ");
+        ConsoleHelper.WriteInConsole(y.ToString(CultureInfo.InvariantCulture), yColor);
+        Console.Write("  |  ");
+        ConsoleHelper.WriteInConsole(z.ToString(CultureInfo.InvariantCulture), zColor);
+        Console.WriteLine();
     }
 
     public void SetNumberByIndex(MoveDto move)
