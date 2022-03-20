428bd94ff14b375c8898f924d2431beeb0651325
Norbert <norbert.petah28@gmail.com>
2022-03-20 16:22:54 +0200

    created a second test project

---
+++ ClientTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace ClientTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
